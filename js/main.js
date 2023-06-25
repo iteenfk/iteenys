5eae4e022709f59c70f9b32339d685283009035e
iteenfk <125781734+iteenfk@users.noreply.github.com>
2023-06-25 14:50:34 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,39 +1,23 @@
 "use strict"
-let seconds = 0;
 
-function A(){
-    let time = document.getElementById("time").value;
-    let unit = document.getElementById("unit").value;
+const volumeSlider = document.getElementById("slider");
+const volumeValue = document.getElementById("vol");
+const audio = document.getElementById("player");
+function volume(){
+    const value = volumeSlider.value;
+    volumeValue.textContent = value + "%";
 
-    if(unit === "秒"){
-        seconds = time;
-    }else if(unit === "分"){
-    seconds = time * 60;
-    }else if(unit === "時間"){
-     seconds = time * 60 * 60;
-    }else if(unit === "日"){
-    seconds = time * 60 * 60 * 24;
-    }else if(unit === "週"){
-     seconds = time * 60 * 60 * 24 * 7;
-    }else if(unit === "月"){
-    seconds = time * 60 * 60 * 24 * 30;
-    }else if(unit === "年"){
-     seconds = time * 60 * 60 * 24 * 365;
-    }
-    const minutes = seconds / 60;
-    const hours = minutes / 60;
-    const days = hours / 24;
-    const weeks = days / 7;
-    const months= days / 30;
-    const years = days / 365;
-
-    document.getElementById("seconds").innerHTML = Number(seconds).toFixed(2) + "秒";
-    document.getElementById("minutes").innerHTML = Number(minutes).toFixed(2) + "分";
-    document.getElementById("hours").innerHTML = Number(hours).toFixed(2) + "時間";
-    document.getElementById("days").innerHTML = Number(days).toFixed(2) + "日";
-    document.getElementById("weeks").innerHTML = Number(weeks).toFixed(2) + "週";
-    document.getElementById("months").innerHTML = Number(months).toFixed(2) + "月";
-    document.getElementById("years").innerHTML = Number(years).toFixed(2) + "年";
+    audio.volume = value / 100;
 }
 
+function play(){
+    audio.play();
+}
 
+function pause(){
+    audio.pause();
+}
+function stop(){
+    audio.pause();
+    audio.currentTime = 0;
+}
